b686fac0506286b7e935c82a958ebf7a06f92033
Vladimir Hanin <s1941432@ed.ac.uk>
2022-03-04 18:40:24 +0100

    added display of frame rate on screen

--- webcam.py
+++ webcam.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 from datetime import datetime
+import time
 
 # * --------- parameters of the module --------- 
 
@@ -12,12 +13,17 @@ def main():
 
     cap = cv.VideoCapture(0)
 
-    # set the frame rate of the capture
-    cap.set(cv.CAP_PROP_FPS, 15)
-
     if not cap.isOpened():
         print("Cannot open camera, it is not connected")
         exit()
+
+
+    # set the webcam speed capture
+    cap.set(cv.CAP_PROP_FPS, 15)
+
+    # variables used to calculate the actual frame rate
+    time_last_frame = time.time()
+    time_current_frame = time.time()
 
     while True:
 
@@ -27,8 +33,12 @@ def main():
             print("Can't receive frame (stream end?). Exiting ...")
             break
 
-        cv.imshow('frame', frame)
-        
+        # display frame rate
+        time_current_frame = time.time()
+        time_elapsed = time_current_frame - time_last_frame
+        frames_per_second = 1 / time_elapsed
+        cv.putText(frame, "FPS: " + str("{:5.2f}".format(frames_per_second)), (10,100), font, 4,(255,255,255),2,cv.LINE_AA)
+        time_last_frame = time_current_frame
 
         # check if user wants to quit
         if cv.waitKey(1) == ord('q'):
@@ -37,6 +47,8 @@ def main():
         # save the current frame if 's' key is pressed
         if cv.waitKey(1) == ord('s'):
             cv.imwrite(DIRECTORY_CAPTURES + "capture " + str(datetime.now()) + ".jpg", frame)
+        
+        cv.imshow('frame', frame)
 
 
     # When everything done, release the capture
