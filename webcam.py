574cb24525da3f264195cfde76b9abc8e64e5269
Vladimir Hanin <s1941432@ed.ac.uk>
2022-03-04 18:13:54 +0100

    added name=main and refactored code

--- webcam.py
+++ webcam.py
@@ -2,37 +2,43 @@ import numpy as np
 import cv2 as cv
 from datetime import datetime
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera, none connected")
-    exit()
-
-
+# * --------- parameters of the capture --------- 
 
 # path to directory where the saved captures should go
 DIRECTORY_CAPTURES = "captures/"
 
-while True:
+def main():
 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    cap = cv.VideoCapture(0)
 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    if not cap.isOpened():
+        print("Cannot open camera, it is not connected")
+        exit()
 
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+    while True:
 
-    # save the current frame is 's' key is pressed
-    if cv.waitKey(1) == ord('s'):
-        cv.imwrite(DIRECTORY_CAPTURES + "capture " + str(datetime.now()) + ".jpg", frame)
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        cv.imshow('frame', frame)
+        
+
+        # check if user wants to quit
+        if cv.waitKey(1) == ord('q'):
+            break
+
+        # save the current frame if 's' key is pressed
+        if cv.waitKey(1) == ord('s'):
+            cv.imwrite(DIRECTORY_CAPTURES + "capture " + str(datetime.now()) + ".jpg", frame)
 
 
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
